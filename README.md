65acfd391578e17f1ff62eba1630207a9471d4a4
Sash Man <sashman90.gmail.com>
2012-05-11 15:59:49 +0100

    added ready command, adds to player count and broadcasts

--- README.md
+++ README.md
@@ -5,10 +5,11 @@ Minecraft mod featuring game play mechanics in the style of Battle Royale
 
 ##### TODO
 
+* Cap server to a specified number of players
+* Once all players send **/ready** message, change state
+
 ###### States
 1. Player lobby stage, players entering
 2. Starting invinciblility stage, players teleported to starting locations
 3. Game stage, players surviving to the last man alive
 
-* Cap server to a specified number of players
-* Once all players send **/ready** message, change state
--- src/com/github/sashman/MC_tehboyz_survival/MC_tehboyz_survival.java
+++ src/com/github/sashman/MC_tehboyz_survival/MC_tehboyz_survival.java
@@ -1,20 +1,44 @@
 package com.github.sashman.MC_tehboyz_survival;
 
+import java.util.List;
 import java.util.logging.Logger;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+
+
+
+
 public class MC_tehboyz_survival extends JavaPlugin{
+	public static final int MAX_PLAYERS = 3;
+	private int player_count = 0;
+	
 	Logger log;
 
 	public void onEnable(){ 
 		log = this.getLogger();
-		log.info("Your plugin has enabled");
+		
 	}
 	 
 	public void onDisable(){ 
-		log.info("Your plugin has disabled");
+
 	}
 	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
+		if(cmd.getName().equalsIgnoreCase("ready")){ // If the player typed /basic then do the following...
+			player_count++;
+			List<Player> players = ((Player)sender).getWorld().getPlayers();
+			for (Player player : players) {
+				player.sendMessage(((Player)sender).getName() + " is ready to play! ("+ player_count + "/" + MAX_PLAYERS+ ")");
+			}
+			
+			return true;
+		} //If this has happened the function will break and return true. if this hasn't happened the a value of false will be returned.
+		return false; 
+	}
 	
 }
