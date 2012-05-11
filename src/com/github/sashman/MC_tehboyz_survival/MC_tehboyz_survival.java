acf07e452005915d74f41c3db6bd5d0f004fabb4
Sash Man <sashman90.gmail.com>
2012-05-12 00:47:40 +0100

    little tidy, added state and state checks

--- src/com/github/sashman/MC_tehboyz_survival/MC_tehboyz_survival.java
+++ src/com/github/sashman/MC_tehboyz_survival/MC_tehboyz_survival.java
@@ -1,13 +1,11 @@
 package com.github.sashman.MC_tehboyz_survival;
 
-import java.awt.HeadlessException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
 import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
-import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -18,18 +16,18 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
-import org.bukkit.event.world.ChunkEvent;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.event.server.MapInitializeEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import com.avaje.ebeaninternal.server.cluster.mcast.Message;
 
 
 
 
 
 public class MC_tehboyz_survival extends JavaPlugin implements Listener {
+	
+	public static enum GameState {Lobby, PreGame, Game};
+	private GameState current_state = GameState.Lobby;
+	
 	public static int MAX_PLAYERS = 2;
 	
 	public static String welcome_msg = "Welcome to the tehboyz survival mod! Type /ready if you are ready to participate";
@@ -45,45 +43,72 @@ public class MC_tehboyz_survival extends JavaPlugin implements Listener {
 
 	public void onEnable(){ 
 		log = this.getLogger();
-		 getServer().getPluginManager().registerEvents(this, this);
+		getServer().getPluginManager().registerEvents(this, this);
 	}
 	 
 	public void onDisable(){ 
 
 	}
 	
+	private void broadcast_msg(List<Player> players, String msg){
+		for (Player player : players) {
+			player.sendMessage(ChatColor.AQUA
+					+ msg);
+		}
+	}
+	
+	
+	
 	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
-		if(cmd.getName().equalsIgnoreCase("ready")){
-			
-			if (sender instanceof Player) {
+	public boolean onCommand(CommandSender sender, Command cmd,
+			String commandLabel, String[] args) {
+
+		switch (current_state) {
+		case Lobby:
+
+			/* Command /ready */
+			if (cmd.getName().equalsIgnoreCase("ready")) {
 				
-				List<Player> players = ((Player) sender).getWorld()
-				.getPlayers();
-				
-				if(!players_playing.contains((Player) sender)){
-					players_playing.add((Player) sender);
-					
-					
-					for (Player player : players) {
-						player.sendMessage(ChatColor.AQUA + ((Player) sender).getName()
-								+ " is ready to play! (" + players_playing.size() + "/"
+				/* Only player can issue */
+				if (sender instanceof Player) {
+					List<Player> players = ((Player) sender).getWorld()
+							.getPlayers();
+					if (!players_playing.contains((Player) sender)) {
+						players_playing.add((Player) sender);
+						broadcast_msg(players,  ((Player) sender).getName()								+ " is ready to play! ("
+								+ players_playing.size() + "/"
 								+ MAX_PLAYERS + ")");
 					}
-				}
-				
-				if(players_playing.size()>= MAX_PLAYERS){
-					for (Player player : players) {
-						player.sendMessage(ChatColor.AQUA + game_start_msg);
+
+					/* If enough players are ready, start the game */
+					if (players_playing.size() >= MAX_PLAYERS) {
+						for (Player player : players) {
+							player.sendMessage(ChatColor.AQUA + game_start_msg);
+						}
 					}
+
 				}
+				return true;
 
 			}
-			return true;
 
-		} // If this has happened the function will break and return true. if this hasn't happened the a value of false will be returned.
-		return false; 
+			break;
+
+		case Game:
+			break;
+
+		default:
+			break;
+		}
+
+		return false;
 	}
+	
+	@EventHandler(priority = EventPriority.HIGH)
+	public void onMapInitializeEvent(MapInitializeEvent event){
+		
+	}
+	
 	
 	@EventHandler(priority = EventPriority.HIGH)
     public void highLogin(PlayerLoginEvent event) {
@@ -92,22 +117,51 @@ public class MC_tehboyz_survival extends JavaPlugin implements Listener {
 	
 	@EventHandler(priority = EventPriority.NORMAL)
 	public void onPlayerJoin(PlayerJoinEvent event) {
-		Player player = event.getPlayer(); 
-        player.sendMessage(ChatColor.AQUA + welcome_msg);
-
-        player.setHealth(player.getMaxHealth());
-        player.setFoodLevel(player.getMaxHealth());
-        player.setGameMode(GameMode.CREATIVE);
+		switch (current_state) {
+		case Lobby:
+			
+			/* Creative mode every joined player  */
+			Player player = event.getPlayer(); 
+	        player.sendMessage(ChatColor.AQUA + welcome_msg);
+	        player.setHealth(player.getMaxHealth());
+	        player.setFoodLevel(player.getMaxHealth());
+	        player.setGameMode(GameMode.CREATIVE);
+			
+			break;
+		case Game:
+			
+			
+			break;
+		default:
+			break;
+		}	
+		
+		
+		
 	}
 	
 	@EventHandler(priority = EventPriority.NORMAL)
 	public void onBlockBreak(BlockBreakEvent event) {
-		event.setCancelled(true);
+		/* Disallow world editing in lobby state*/
+		switch (current_state) {
+		case Lobby:
+			event.setCancelled(true);
+			break;
+		default:
+			break;
+		}
 	}
 	
 	@EventHandler(priority = EventPriority.NORMAL)
 	public void onBlockPlaceEvent(BlockPlaceEvent event) {
-		event.setCancelled(true);
+		/* Disallow world editing in lobby state*/
+		switch (current_state) {
+		case Lobby:
+			event.setCancelled(true);
+			break;
+		default:
+			break;
+		}
 	}
 	
 }
