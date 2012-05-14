70d3eed3ec242b905fd7031587caba9d35cdd3e0
Hugh Cannon <hugh@hughcannon.com>
2012-05-14 21:48:05 +0100

    Removed sysout, created separate config class

--- src/com/github/sashman/MC_tehboyz_survival/MC_tehboyz_survival.java
+++ src/com/github/sashman/MC_tehboyz_survival/MC_tehboyz_survival.java
@@ -1,18 +1,8 @@
 package com.github.sashman.MC_tehboyz_survival;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.logging.Logger;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
@@ -35,10 +25,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class MC_tehboyz_survival extends JavaPlugin implements Listener {
 	
-	private static final String CONFIG_LOCATION = "plugins/survival.config";
-	private static final String[] CONFIG_KEYS = new String[]{	"MAX_PLAYERS", 
-																"WORLD_SIZE", 
-																"BOUNDS_CHANGE_TIME"};
+	public static final String CONFIG_LOCATION = "plugins/survival.config";
 	
 	public static enum GameState {Lobby, PreGame, Game};
 	private GameState current_state = GameState.Lobby;
@@ -68,94 +55,15 @@ public class MC_tehboyz_survival extends JavaPlugin implements Listener {
 	Logger log;
 
 	public void onEnable(){
-		readFromConfig();
-		
 		log = this.getLogger();
 		getServer().getPluginManager().registerEvents(this, this);
+		
+		Config.readFile(this);
 	}
 	
 	
-	/* Reads from config file. 
-	 * If file does not exist then creates and writes default values to file
-	 * Checks for presence of each of the config keys. If any are missing from the file
-	 * then they are written to the file with default values and alerted to console.
-	 */
-	private void readFromConfig() {
-		File f = new File(CONFIG_LOCATION);
-		String pattern = "^[a-zA-Z0-9_]+\\s+[a-zA-Z0-9]+$"; //Matches key<whitespace>val
-		HashSet<String> keys = new HashSet<String>();
-		for(String key: CONFIG_KEYS){
-			keys.add(key);
-		}
-		
-		try {
-			
-			BufferedReader br = new BufferedReader(new FileReader(f));
-			String s = br.readLine();
-			System.out.println("Reading config file.");
-			while(s != null){
-				if(Pattern.matches(pattern, s)){
-					String[] keyval = s.split(" ");
-					//TODO need to do verification of values
-					if(keys.contains(keyval[0])){
-						keys.remove(keyval[0]);
-						
-						if(keyval[0].equals("MAX_PLAYERS"))
-							MAX_PLAYERS = new Integer(keyval[1]);
-						if(keyval[0].equals("WORLD_SIZE"))
-							WORLD_SIZE = new Integer(keyval[1]);
-						if(keyval[0].equals("BOUNDS_CHANGE_TIME"))
-							BOUNDS_CHANGE_TIME = new Integer(keyval[1]);
-					}else{
-						System.out.println("Key: '" + keyval[0] + "' not recognised.");
-					}
-				}
-				s = br.readLine();
-			}
-			
-		} catch (FileNotFoundException e) {
-			System.out.println("Config file not found at :'" + f.getAbsolutePath() + "'. Creating file and using default values.");
-			try {
-				f.createNewFile();
-			} catch (IOException e1) {
-				System.out.println("Unable to create config file at: " + f.getAbsolutePath());
-			}
-		} catch (IOException e) {
-			System.out.println("Unable to read from config file. Using default values.");
-		}
-		
-		if(!keys.isEmpty()){
-			BufferedWriter bw;
-			try {
-				bw = new BufferedWriter(new FileWriter(f));
-				System.out.println("The following keys were not in the config file:");
-				for(String k: keys){
-					System.out.println(k);
-					if(k.equals("MAX_PLAYERS"))
-						bw.write(k + " " + MAX_PLAYERS + "\n");
-					if(k.equals("WORLD_SIZE"))
-						bw.write(k + " " + WORLD_SIZE + "\n");
-					if(k.equals("BOUNDS_CHANGE_TIME"))	
-						bw.write(k + " " + BOUNDS_CHANGE_TIME + "\n");
-				}
-				bw.flush();
-			} catch (IOException e) {
-				System.out.println("Unable to write to config file at: " + f.getAbsolutePath());
-			}
-		}
-		System.out.println("Reading from config file complete");
-		System.out.println("Values:");
-		for(String k: CONFIG_KEYS){
-			if(k.equals("MAX_PLAYERS"))
-				System.out.println(k + "=" + MAX_PLAYERS);
-			if(k.equals("WORLD_SIZE"))
-				System.out.println(k + "=" + WORLD_SIZE);
-			if(k.equals("BOUNDS_CHANGE_TIME"))	
-				System.out.println(k + "=" + BOUNDS_CHANGE_TIME);
-		}
-		
-	}
-
+	
+	
 	public void onDisable(){ 
 
 	}
